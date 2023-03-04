44912039280122d5aef04ba0b4da1007d675bbe6
Victoria-Cimpu <vcimp069@uottawa.ca>
2023-03-04 18:10:08 -0500

    Formatting score
    Put score in Minecraft font and moved it up to the right upper corner.

--- Assets/ScoreManager.cs
+++ Assets/ScoreManager.cs
@@ -18,6 +18,6 @@ public class ScoreManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        scoreText.text = "Score:" + SCORE;
+        scoreText.text = "Score: " + SCORE;
     }
 }
