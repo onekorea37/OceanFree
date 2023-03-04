3b875c0b79597fda1c43a2b4946c08c05cfd4c27
Victoria-Cimpu <vcimp069@uottawa.ca>
2023-03-04 13:53:12 -0500

    Added spawner for bouncing ball

---
+++ Assets/RandomSpawner.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomSpawner : MonoBehaviour
+{
+    public GameObject spherePrefab;
+    public float minX = -9, maxX = 9, minY = -4, maxY =0;
+    public float minSpawnInterval = 10, maxSpwanInterval = 20;
+
+
+    void Start()
+    {
+        StartCoroutine(SpawnBlocks());
+    }
+
+    IEnumerator SpawnBlocks() 
+    {
+        while(true) 
+        {
+            Vector2 randomPosition = new Vector2(
+                Random.Range(minX, maxX),
+                Random.Range(minY, maxY)
+            ); 
+
+            Instantiate(spherePrefab, randomPosition, Quaternion.identity);
+
+        
+            yield return new WaitForSeconds(Random.Range(minSpawnInterval, maxSpwanInterval));
+        }
+    }
+    /*
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SpawnObject();
+    }
+
+    void SpawnObject() {
+        Vector2 randomPosition = new Vector2(
+            Random.Range(minX, maxX),
+            Random.Range(minY, maxY)
+        ); 
+
+        Instantiate(spherePrefab, randomPosition, Quaternion.identity);
+    }*/
+}
