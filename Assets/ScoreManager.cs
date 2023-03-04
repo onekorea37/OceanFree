33e03518fe64a3de1f218cbfcedb23630cf26208
Victoria-Cimpu <vcimp069@uottawa.ca>
2023-03-04 18:59:05 -0500

    Added messages depending on score
    Messages will appear when the player reaches a certain score and disappear after 15 sec.

--- Assets/ScoreManager.cs
+++ Assets/ScoreManager.cs
@@ -9,15 +9,52 @@ public class ScoreManager : MonoBehaviour
 
     public TextMeshProUGUI scoreText;
 
+    public GameObject msg1, msg2, msg3, msg4, msg5;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        msg1.SetActive(false);
+        msg2.SetActive(false);
+        msg3.SetActive(false);
+        msg4.SetActive(false);
+        msg5.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
         scoreText.text = "Score: " + SCORE;
+
+        if (SCORE == 10) {
+            msg1.SetActive(true);
+            StartCoroutine(WaitForSec(msg1));
+        }
+
+        if (SCORE == 20) {
+            msg2.SetActive(true);
+            StartCoroutine(WaitForSec(msg2));
+        }
+
+        if (SCORE == 30) {
+            msg3.SetActive(true);
+            StartCoroutine(WaitForSec(msg3));
+        }
+
+        if (SCORE == 50) {
+            msg4.SetActive(true);
+            StartCoroutine(WaitForSec(msg4));
+        }
+
+        if (SCORE == 80) {
+            msg5.SetActive(true);
+            StartCoroutine(WaitForSec(msg5));
+        }
+    }
+
+    IEnumerator WaitForSec(GameObject msg) 
+    {
+        yield return new WaitForSeconds(15);
+        Destroy(msg);
     }
 }
